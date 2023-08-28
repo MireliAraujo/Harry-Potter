e2a4d2f576ba5a8c572976d027ee218d3cc4b071
Mireli Araujo <112290519+jonascipriano48@users.noreply.github.com>
2023-08-28 19:15:40 -0300

    organização de pastas

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
 
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./build/styles'));
+        
+}
+
+function comprimeImages() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
+}
+
+
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial:false }, gulp.series(compilaSass));
+    gulp.watch('./source/images/*', { ignoreInitial:false }, gulp.series(comprimeImages));
+}
